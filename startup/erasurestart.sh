669f6ec527d157df9b0b220b6110751903932589
Yashraj Patil <81092460+yashrajpatil253@users.noreply.github.com>
2023-11-02 15:13:37 +0530

    Add files via upload

---
+++ startup/erasurestart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /opt/.xcaliber/installer
+bash erasure_installation_verification.sh
+
+#sleep 10 && cd /opt/.xcaliber/ota/
+#bash ota_main_file.sh &
+
+#cd /opt/.xcaliber/erasure_midas/sqrt/src/
+#python3 -u erasure_socket.py &> /opt/.xcaliber/logs/erasure-socket.log &
+
+#cd /opt/.xcaliber/erasure_midas/erasure_packages/
+#./Erasure &> /opt/.xcaliber/logs/erasure-flask.log &
+
