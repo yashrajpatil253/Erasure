6dcd4715d51ac68331ff9111d6e8a3d6c024be2f
Yashraj Patil <81092460+yashrajpatil253@users.noreply.github.com>
2023-11-07 17:03:48 +0530

    Update erasurestart.sh

--- startup/erasurestart.sh
+++ startup/erasurestart.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#kasa kay
+
 cd /opt/.xcaliber/installer
 bash erasure_installation_verification.sh
-#run ho jayega na bhai abb ghar jana he
+
 #sleep 10 && cd /opt/.xcaliber/ota/
 #bash ota_main_file.sh &
 
@@ -11,5 +11,5 @@ bash erasure_installation_verification.sh
 
 #cd /opt/.xcaliber/erasure_midas/erasure_packages/
 #./Erasure &> /opt/.xcaliber/logs/erasure-flask.log &
-#game djsbdjjdjhdhudcd
+# ye wala parametrized build he 
 
