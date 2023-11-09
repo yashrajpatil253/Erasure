a7a56646f4ed8ca8ac7ece0734f1685c32e3ceb3
Yashraj Patil <81092460+yashrajpatil253@users.noreply.github.com>
2023-11-09 12:46:28 +0530

    Update truwipe_start.sh

--- startup/truwipe_start.sh
+++ startup/truwipe_start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-for pid in `ps -aef | grep -E 'python3|usbmuxd|Erasure' | awk '{print $2}'` ; do
-sudo kill -9 $pid
-done
-
-cd /opt/.xcaliber/erasure_midas/erasure_packages/
-./Erasure &> /opt/.xcaliber/logs/erasure-flask.log &
-
-#cd /opt/.xcaliber/midas-hub-device-agent/
-#python3 -u main.py &> /opt/.xcaliber/logs/hub.log &
 
 
 
